1e195efbdfc0a1cd43cf145dd938cee6a47e08d1
Christopher Yeung <chris.yeung@cantab.net>
2024-08-09 15:52:04 +0100

    Added pre-condition check

--- src/main/java/com/adaton/model/InstrumentPrice.java
+++ src/main/java/com/adaton/model/InstrumentPrice.java
@@ -1,10 +1,22 @@
 package com.adaton.model;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.time.LocalDate;
+import java.util.Objects;
 
 public record InstrumentPrice(
         String vendorId,
         String instrumentId,
         LocalDate priceDate,
-        Double price) {
+        double price) {
+
+    public InstrumentPrice {
+        if (StringUtils.isEmpty(vendorId))
+            throw new IllegalArgumentException("vendorId cannot be empty");
+        if (StringUtils.isEmpty(instrumentId))
+            throw new IllegalArgumentException("instrumentId cannot be empty");
+        Objects.requireNonNull(priceDate);
+    }
+
 }
